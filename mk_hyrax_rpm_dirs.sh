1df98516dc21e1aff7974a6ff53381a04a7fff4c
James Gallagher <jgallagher@opendap.org>
2011-06-09 00:20:35 +0000

    More features
    M    mk_hyrax_rpm_dirs.sh

--- mk_hyrax_rpm_dirs.sh
+++ mk_hyrax_rpm_dirs.sh
@@ -3,13 +3,13 @@
 # Make the directories for a $dist os for the hyrax RPMs
 #
 # How to use this: Make the rpms; sign the rpms; transfer the RPMs to
-# the web site and put them in the pub/binary/tmp directory. Then run
-# the script on the web site in the pub/binary directory and enter the
-# component version numbers at the prompt. 
-#
-# I've added this to the shrew project so the script doesn't get lost.
+# this machine and put them in the pub/binary/tmp directory. Then run
+# the script and enter the component version numbers at the prompt.
 #
 # jhrg 9/17/2010
+
+do_32_bit="yes"
+do_64_bit=
 
 dist=centos5.2
 x32=i386
@@ -19,41 +19,53 @@ pub=/var/www/web_site/pub/binary
 # Put the rpms and sig files here before running this script
 tmp=$pub/tmp
 
-parts="libdap bes hdf4_handler dap-server hdf5_handler fileout_netcdf \
-ncml_module freeform_handler netcdf_handler"
+parts="bes csv_handler dap-server fileout_netcdf freeform_handler \
+    gateway_module hdf4_handler hdf5_handler libdap ncml_module netcdf_handler"
+
+# parts="libdap bes hdf4_handler dap-server hdf5_handler fileout_netcdf \
+# ncml_module freeform_handler netcdf_handler"
 
 for p in $parts
-do
-  read -p "$p version: " ver junk
+  do
+  # Read the version numbers using the files and a pattern
+  f=`ls $tmp/$p-[0-9]*.rpm`
+  ver=`echo $f | sed "s@.*-\([0-9]*\.[0-9]*\.[0-9]*\)[-.].*@\1@"`
+
+  # Old way: read the numbers manually
+  # read -p "$p version: " ver junk
 
   echo "ver: $ver"
-  # handler the 32-bit case
+  # handle the 32-bit case
+  if test -n "$do_32_bit"
+      then
+      d=$p/$dist/${ver}_$x32
+      if test ! -d $d
+	  then
+	  echo "mkdir -p $d"
+	  mkdir -p $d
+      else
+	  echo "$d exists"
+      fi
 
-  d=$p/$dist/${ver}_$x32
-  if test ! -d $d
-  then
-      echo "mkdir -p $d"
-      mkdir -p $d
-  else
-      echo "$d exists"
+      echo "Copying $tmp/$p*-$ver-?.$x32.rpm* to $d"
+      cp $tmp/$p*-$ver-?.$x32.rpm* $d
   fi
 
-  echo "Copying $tmp/$p*-$ver-?.$x32.rpm* to $d"
-  cp $tmp/$p*-$ver-?.$x32.rpm* $d
 
   # now do the 64-bit case
+  if test -n "$do_64_bit"
+      then
+      d=$p/$dist/${ver}_$x64
+      if test ! -d $d
+	  then
+	  echo "mkdir -p $d"
+	  mkdir -p $d
+      else
+	  echo "$d exists"
+      fi
 
-  d=$p/$dist/${ver}_$x64
-  if test ! -d $d
-  then
-      echo "mkdir -p $d"
-      mkdir -p $d
-  else
-      echo "$d exists"
+      echo "Copying $tmp/$p*-$ver-?.$x64.rpm* to $d"
+      cp $tmp/$p*-$ver-?.$x64.rpm* $d
   fi
-
-  echo "Copying $tmp/$p*-$ver-?.$x64.rpm* to $d"
-  cp $tmp/$p*-$ver-?.$x64.rpm* $d
-
 done
 

6acacff2ba03ab9bde9862c97876bf2208cda00b
James Gallagher <jgallagher@opendap.org>
2011-08-02 20:05:47 +0000

    Made the script a bit more universal.
    M    vmbuild_hyrax_i386.sh

--- vmbuild_hyrax_i386.sh
+++ vmbuild_hyrax_i386.sh
@@ -3,20 +3,23 @@
 # Use vmware virtual machine for the nightly builds. Assume that the code
 # has been already checked out and the nbuild script set up.
 
-export PATH=/Library/Application\ Support/VMware\ Fusion/:$PATH
+# Not needed for linux hosts
+# export PATH=/Library/Application\ Support/VMware\ Fusion/:$PATH
 
-export host_type=fusion
-export guest=/Users/jimg/OPeNDAP_Build_CentOS_5.2_i386.vmwarevm
+# export host_type="-T fusion"
+export host_type=
+# export guest=/Users/jimg/OPeNDAP_Build_CentOS_5.2_i386.vmwarevm
+export guest=/home/jimg/vmware/CentOS_5.6_32-bit/CentOS_5.6_32-bit.vmx
 export guest_user=opendap
-export guest_pass=build
+export guest_pass=builds
 
 # If the VMware gui thing is already running, the we have to use 'gui' with
 # start or vmrun reports an error that the "Error: The file is already in use"
 # I think that 'nogui' only works when the VMware application is not running.
 
-vmrun -T $host_type start $guest nogui
+vmrun $host_type start $guest nogui
 
-vmrun -T $host_type -gu $guest_user -gp $guest_pass runScriptInGuest $guest \
+vmrun $host_type -gu $guest_user -gp $guest_pass runScriptInGuest $guest \
 /bin/sh "cd /home/opendap/hyrax_1.7_release && sh -vx ./nbuild_shrew.sh > nbuild.log 2>&1"
 
-vmrun -T $host_type suspend $guest soft
+vmrun $host_type suspend $guest soft

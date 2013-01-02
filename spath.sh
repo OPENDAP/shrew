9a6bf9121b38f0514af575d14c9e9f8e614af8b1
James Gallagher <jgallagher@opendap.org>
2013-01-02 16:12:38 +0000

    Fixed issue on OSX where tomcat was not found when both the tar.gz and expanded file are in shrew.
    M    spath.sh

--- spath.sh
+++ spath.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 #
 # Source this in the directory to be 'prefix' or pass the directory
 # as the first param. Make this a command using 'alias.' e.g.
@@ -13,6 +13,7 @@ fi
 
 export orig_path=$PATH
 
+# Set 'prefix' to either the first argument or the current working directory.
 # Can't use ${1:-...}; positional params don't work in the ${:-} syntax
 prefix=$1
 export prefix=${prefix:-$PWD}
@@ -20,6 +21,7 @@ export PATH=$prefix/bin:$PATH
 
 # This is needed for the linux builds; if using the deps libraries
 # on linux, those directories also need to be on LD_LIBRARY_PATH.
+# I'mm not sure this is true... jhrg 1/2/13
 export LD_LIBRARY_PATH=$prefix/lib
 
 # Find tomcat - it might not be installed in $prefix yet.
@@ -29,20 +31,19 @@ unset tc
 export tc=`ls -1 $prefix/src/dependencies/downloads/apache-tomcat-*`
 
 unset tcp
-export tcp=`ls -1d $prefix/apache-tomcat-[0-9]*.[0-9]*.[0-9]*`
+# export tcp=`ls -1d $prefix/apache-tomcat-[0-9]*.[0-9]*.[0-9]*`
+export tcp=`ls -1 -d $prefix/apache-tomcat-* | grep 'apache-tomcat-[0-9]*\.[0-9]*\.[0-9]*$'`
 
 if test -d $tcp
 then
     export TOMCAT_DIR=$tcp
+    export CATALINA_HOME=$TOMCAT_DIR
 elif test -f $tc
 then
     tc=${tc##/*/}
     tc=${tc%.tar.gz}
     export TOMCAT_DIR=$prefix/$tc
+    export CATALINA_HOME=$TOMCAT_DIR
 else
     echo "Can't find tomcat..."
 fi
-
-# Added because references to CATALINA_HOME will determine which
-# copy of tomcat ./apache-tomcat-*/bin/startup.sh actually starts.
-export CATALINA_HOME=$TOMCAT_DIR

8e17933d80be9cf9f766ea7f7986887e9a1b3185
James Gallagher <jgallagher@opendap.org>
2013-06-26 23:27:08 +0000

    Wired up the branches for bes and libdap in this shrew/branch for MIIC.
    _M   .
    M    externals.txt
    M    set_externals.sh

--- set_externals.sh
+++ set_externals.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 #
-# NB: I'm not sure if this is used anymore. See the trac page about
-# "Making a Branch of Shrew for a Server Release"jhrg 8/1/11 
+# Read the values for the svn externals property from a text file and 
+# set it.
 
-while read svn_dir src_dir
-do
-    rev=`svn info $src_dir | grep Revision | cut -d ' ' -f 2`
-    # echo $svn_dir
-    # echo $src_dir
-    # echo $rev
-    echo "$svn_dir@$rev $src_dir"
-done
+if test -n "$1"
+then
+    ext="$1"
+else
+    ext="externals.txt"
+fi
+
+svn propset svn:externals --file $ext .
